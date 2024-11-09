5f015f6659b7c3a8a463070f8c81c42f74759af3
Tai Keith <keithsteve.ks@hotmail.com>
2024-11-10 01:04:36 +0300

    Implemented a hash_password function that expects one string argument name password and returns a salted, hashed password, which is a byte string.

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that expects one string argument name 
+'password' and returns a salted, hashed password, which is a byte string.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hash a password with a salt using bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The salted, hashed password.
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
