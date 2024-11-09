a2d12c5aa70686c5f80e473dfb4e84a93e74150d
Tai Keith <keithsteve.ks@hotmail.com>
2024-11-10 01:13:46 +0300

    Implemented an is_valid function that expects 2 arguments and returns a boolean.

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -19,3 +19,17 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate that the provided password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): The hashed password.
+        password (str): The password to validate.
+
+    Returns:
+        bool: True if the password matches the hash, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
