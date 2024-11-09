565af15806dab38036da036f4aec83b9388ee479
Tai Keith <keithsteve.ks@hotmail.com>
2024-11-09 23:40:22 +0300

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Tasks module
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -26,3 +27,23 @@ def filter_datum(fields: List[str], redaction: str,
     return re.sub(pattern,
                   lambda m: f"{m.group().split('=')[0]}={redaction}",
                   message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize with fields to redact"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the log record with redacted sensitive fields"""
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
