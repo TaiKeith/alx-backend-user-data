d1515e8a803b03d736c1268baf967e9b1b548538
Tai Keith <keithsteve.ks@hotmail.com>
2024-11-11 14:09:45 +0300

    Created a class Auth class to manage the API authentication.

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This module contains a class to manage the API authentication.
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Manages the API authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Determines if authentication is required """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Returns the authorization header """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Returns the current user """
+        return None
