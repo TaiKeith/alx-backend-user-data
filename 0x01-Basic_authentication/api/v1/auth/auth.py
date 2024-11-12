08ec4089a72472a57ec0b65cd7de349d549c54e2
Tai Keith <keithsteve.ks@hotmail.com>
2024-11-12 15:45:25 +0300

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ This module contains a class to manage the API authentication.
 """
 from flask import request
 from typing import List, TypeVar
+import fnmatch
 
 
 class Auth:
@@ -11,18 +12,34 @@ class Auth:
     Manages the API authentication.
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Determines if authentication is required """
-        if path is None:
+        """
+        Determines if authentication is required for the given path based on
+        the excluded paths list.
+
+        Args:
+            path (str): The path to check.
+            excluded_paths (list): The list of paths or patterns
+                                   (with possible wildcards) that are excluded
+                                   from requiring authentication.
+
+        Returns:
+            bool: True if authentication is required,
+                  False if it's excluded by the patterns.
+        """
+        if path is None or excluded_paths is None:
             return True
-        if not excluded_paths:
-            return True
+
+        for pattern in excluded_paths:
+            if fnmatch.fnmatch(path, pattern):
+                return False
+        return True
 
         # Ensure path ends with a slash for comparison
-        if not path.endswith('/'):
-            path += '/'
+        # if not path.endswith('/'):
+        # path += '/'
 
         # Check if the normalized path is in excluded_paths
-        return path not in excluded_paths
+        # return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """ Returns the authorization header """
