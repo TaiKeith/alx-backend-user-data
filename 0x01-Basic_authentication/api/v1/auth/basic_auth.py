8bf35e2c7cc544bfe08c780b8e5f3663bcf6f0af
Tai Keith <keithsteve.ks@hotmail.com>
2024-11-12 14:58:34 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,12 +12,14 @@ class BasicAuth(Auth):
         """
         Extracts the Base64 part of the Authorization header for Basic
         Authentication.
-        
+
         Args:
-            authorization_header (str): The Authorization header from the request.
-        
+            authorization_header (str): The Authorization header from
+                                        the request.
+
         Returns:
-            str: The Base64 part of the Authorization header, or None if invalid.
+            str: The Base64 part of the Authorization header,
+                 or None if invalid.
         """
         if authorization_header is None:
             return None
