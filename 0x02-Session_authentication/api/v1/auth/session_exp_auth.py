fb880d4a3fc674d77796d0c13ea8cbc90a1f32b4
Tai Keith <keithsteve.ks@hotmail.com>
2024-11-14 14:20:50 +0300

    Enhanced code readability

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -36,6 +36,8 @@ class SessionExpAuth(SessionAuth):
         session_id = super().create_session(user_id)
         if session_id is None:
             return None
+
+        # Store the session with user ID and creation time
         session_dictionary = {
             "user_id": user_id,
             "created_at": datetime.now()
@@ -53,14 +55,21 @@ class SessionExpAuth(SessionAuth):
         """
         if session_id is None:
             return None
+
         user_details = self.user_id_by_session_id.get(session_id)
         if user_details is None:
             return None
-        if "created_at" not in user_details.keys():
+
+        # Ensure session has a creation time
+        created_at = user_details.get("created_at")
+        if created_at is None:
             return None
+
+
         if self.session_duration <= 0:
             return user_details.get("user_id")
-        created_at = user_details.get("created_at")
+
+        # Check if session has expired
         allowed_window = created_at + timedelta(seconds=self.session_duration)
         if allowed_window < datetime.now():
             return None
