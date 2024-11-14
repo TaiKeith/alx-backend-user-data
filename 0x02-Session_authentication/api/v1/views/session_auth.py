98bfb5ff4a4ea0eb9be266bbd8944b90c8491ed1
Tai Keith <keithsteve.ks@hotmail.com>
2024-11-14 13:37:28 +0300

    Made some minor updates

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -45,11 +45,13 @@ def login():
 
 @app_views.route('/auth_session/logout', methods=['DELETE'],
                  strict_slashes=False)
-def handle_logout():
+def logout():
     """
-    Handle user logout
+    DELETE /auth_session/logout:
+        Logs out a user by destroying their session.
     """
     from api.v1.app import auth
+
     if auth.destroy_session(request):
         return jsonify({}), 200
     abort(404)
