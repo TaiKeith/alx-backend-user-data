1fa3f5a4d5b8498595b6281bc6c1551347c8a382
Tai Keith <keithsteve.ks@hotmail.com>
2024-11-20 18:00:42 +0300

    Implemented a update_password function that responds to the PUT /reset_password route.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -113,5 +113,25 @@ def get_reset_password_token() -> str:
     return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
+@app.route('/reset_password', methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """
+    Handles the password update request.
+
+    Returns:
+        JSON response confirming the update or raises a 403 error.
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": f"{email}", "message": "Password updated"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
